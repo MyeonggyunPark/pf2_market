4b09ddbb7f5f61cdd8d1d66ce10356062588083b
Myeonggyun Park <audrbs92@gmail.com>
2025-11-17 18:25:46 +0100

    chore: configure AUTH_USER_MODEL to use market.User

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Instruct Django to use the custom User model defined in market/models.py
+AUTH_USER_MODEL = "market.User"
