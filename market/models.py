bd3ebe7b596cadd692d48836aed74a260486cf27
Myeonggyun Park <audrbs92@gmail.com>
2025-11-17 18:25:25 +0100

    feat: define custom User model based on AbstractUser

--- market/models.py
+++ market/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Reusable base class for a fully featured User model
+from django.contrib.auth.models import AbstractUser
+
+
+
+# Custom user model extending Django's default AbstractUser
+class User(AbstractUser):
+    pass
