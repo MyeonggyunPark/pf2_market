e75cc61e635586fc91baca2cd4ff697811851ff3
Myeonggyun Park <audrbs92@gmail.com>
2025-11-18 10:25:49 +0100

    feat: Add optional unique nickname field to User model

--- market/models.py
+++ market/models.py
@@ -4,9 +4,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-
 # Custom user model extending Django's default AbstractUser
 class User(AbstractUser):
-    
+
+    # Custom optional unique nickname field for each user
+    nickname = models.CharField(max_length=15, unique=True, null=True)
+
     def __str__(self):
         return self.email
