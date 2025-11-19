013243b3987ef44bfefe0cf431237db43c1f578e
Myeonggyun Park <audrbs92@gmail.com>
2025-11-19 18:23:48 +0100

    chore: enable django humanize app for template filters

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     # Required by django-allauth to tie users to a Site object
     "django.contrib.sites",
     # Third-party apps
